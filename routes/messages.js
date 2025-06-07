82bd54217adf31f35fea8ac201bb5e93e37b5b25
Jaskaransingh098 <jaskarandosanjh88@gmail.com>
2025-06-07 12:33:57 +0530

    initial backend commit

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('./path-to-authenticateToken'); // adjust path
+const authenticateToken = require('../middleware/authenticateToken'); // adjust path
 
 let messages = [
     { sender: 'User1', recipient: 'User2', message: 'Hello!', timestamp: new Date() },
