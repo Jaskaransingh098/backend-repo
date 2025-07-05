bef7cccefb7adeab499844af981d549198e11c24
Jaskaransingh098 <jaskarandosanjh88@gmail.com>
2025-07-05 23:36:32 +0530

    initial backend commit

--- routes/auth.js
+++ routes/auth.js
@@ -8,19 +8,19 @@ const authenticateToken = require('../middleware/authenticateToken')
 const JWT_SECRET =
     process.env.JWT_SECRET;
 
-    router.get('/me', authenticateToken, async (req, res) => {
-        try {
-            // Find user by ID (stored in token payload) and return relevant details
-            const user = await User.findById(req.user.id).select('username isPro activePlan'); // Adjust this to return required fields
-            if (!user) {
-                return res.status(404).json({ msg: 'User not found' });
-            }
-            res.json(user); // Send user data back to frontend
-        } catch (err) {
-            console.error("Error fetching user data:", err);
-            res.status(500).json({ msg: "Failed to fetch user data" });
+router.get('/me', authenticateToken, async (req, res) => {
+    try {
+        // Find user by ID (stored in token payload) and return relevant details
+        const user = await User.findById(req.user.id).select('username isPro activePlan'); // Adjust this to return required fields
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
         }
-    });
+        res.json(user); // Send user data back to frontend
+    } catch (err) {
+        console.error("Error fetching user data:", err);
+        res.status(500).json({ msg: "Failed to fetch user data" });
+    }
+});
 
 router.post('/signup', async (req, res) => {
     const { username, password, email, isPro } =
@@ -47,6 +47,22 @@ router.post('/signup', async (req, res) => {
     }
 
 });
+router.get('/check-username/:username', async (req, res) => {
+    const { username } = req.params;
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.json({ exists: true });
+        } else {
+            return res.json({ exists: false });
+        }
+    } catch (err) {
+        console.error("Username check error:", err);
+        return res.status(500).json({ msg: "Server error" });
+    }
+});
 
 router.post('/login', async (req, res) => {
     const { username, password } =
@@ -60,7 +76,7 @@ router.post('/login', async (req, res) => {
         if (!isMatch) {
             return res.status(400).json({ msg: "Invalid credentials " })
         }
-        const token = jwt.sign({ id: user._id, username: user.username, isPro:user.isPro, activePlan: user.activePlan }, JWT_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ id: user._id, username: user.username, isPro: user.isPro, activePlan: user.activePlan }, JWT_SECRET, { expiresIn: "1h" });
         res.json({ token });
     }
     catch (err) {
